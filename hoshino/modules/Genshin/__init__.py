c222b5b6bc71d55b3490f339dd55ef541c252a57
eighteen18 <736458622@qq.com>
2021-04-18 14:40:28 +0800

    菜单

---
+++ hoshino/modules/Genshin/__init__.py
@@ -0,0 +1,27 @@
+from hoshino import Service
+
+
+sv = Service("原神帮助")
+
+
+help_txt = '''
+「卢皮卡」想要了解我，我会的，不多。
+https://shimo.im/docs/9RwRHRyqk8ptgCRt
+'''
+
+
+@sv.on_fullmatch("雷泽帮助")
+async def help1(bot, ev):
+    await bot.send(ev, help_txt)
+
+@sv.on_fullmatch("雷泽功能")
+async def help2(bot, ev):
+    await bot.send(ev, help_txt)
+
+@sv.on_fullmatch("雷泽菜单")
+async def help3(bot, ev):
+    await bot.send(ev, help_txt)
+
+@sv.on_fullmatch("雷泽会什么")
+async def help4(bot, ev):
+    await bot.send(ev, help_txt)
